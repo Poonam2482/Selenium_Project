39c1ebf8264b814177ef454eccfda67105136352
Poonam2482 <66667170+Poonam2482@users.noreply.github.com>
2021-03-21 11:23:45 +0530

    changes

--- ConsoleApp1/Login.cs
+++ ConsoleApp1/Login.cs
@@ -21,11 +21,7 @@ using System.Collections.ObjectModel;
 namespace ConsoleApp1
 {
     [TestClass]
-<<<<<<< HEAD
     public class Login
-=======
-    class Login
->>>>>>> 0584fe9f11aee0a790ea445d525a21380e12d57e
     {
         IWebDriver m_driver;
         DataTable dtLog = new DataTable();
